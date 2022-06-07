af3fc4fa43731ca1e08e990508876da1c517044a
John L. Whiteman <>
2022-06-07 10:19:00 -0700

    demo tweaks
    Signed-off-by: John L. Whiteman <>

--- demo/demo.sh
+++ demo/demo.sh
@@ -3,14 +3,15 @@ echo "----------------------------------------"
 echo -e "[THE GOOD]\n"
 echo -e "$ python ../auditor.py ../models/good.json\n"
 python ../auditor.py ../models/good.json
-sleep 3
+sleep 2 
 echo "----------------------------------------"
 echo -e "[THE BAD]\n"
 echo -e "$ python ../auditor.py ../models/bad.json\n"
 python ../auditor.py ../models/bad.json
-sleep 5
+sleep 3 
 echo "----------------------------------------"
 echo -e "[THE UGLY]\n"
 echo -e "$ python ../auditor.py ../models/ugly.json\n"
 python ../auditor.py ../models/ugly.json
 echo "----------------------------------------"
+sleep 1 
